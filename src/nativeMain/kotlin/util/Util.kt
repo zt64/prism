d0d994639e94043861979fc7b39e3c660a93823a
keli5 <46039037+keli5@users.noreply.github.com>
2022-08-01 14:40:36 -0500

    Address  comments 1, 2, 3

--- src/nativeMain/kotlin/util/Util.kt
+++ src/nativeMain/kotlin/util/Util.kt
@@ -10,7 +10,6 @@ import xlib.*
 
 fun getConfigDir(): String? = getenv("XDG_CONFIG_HOME")?.toKString() ?: getenv("HOME")?.toKString()?.plus("/.config")
 fun Boolean.toInt(): Int = if (this) 1 else 0
-var windowCursors: MutableMap<Window, Cursor> = mutableMapOf()
 var windowCursorInitialized: MutableMap<Window, Boolean> = mutableMapOf()
 
 fun Display.sendClientMessage(type: Atom, window: Window, propagate: Boolean = false, eventMask: Long = NoEventMask, vararg data: Long) {
@@ -35,19 +34,16 @@ fun Display.sendClientMessage(type: Atom, window: Window, propagate: Boolean = f
 fun Display.sendClientMessage(type: Atom, propagate: Boolean = false, eventMask: Long = NoEventMask, vararg data: Long) =
     sendClientMessage(type, rootWindow, propagate, eventMask, *data)
 
-fun Window.setCursor(dpy: Display, cur: UInt = 2.toUInt(), window: Window = this) { 
-    var cursorInitialized = windowCursorInitialized[window]
+public fun Window.setCursor(dpy: Display, cur: UInt = 2.toUInt()) { 
+    var cursorInitialized = windowCursorInitialized[this]
     if (cursorInitialized != true) {
-        windowCursors.put(window, XCreateFontCursor(dpy.ptr, cur))
-        XDefineCursor(dpy.ptr, window, windowCursors[window]!!)
-        windowCursorInitialized.put(window, true)
+        XDefineCursor(dpy.ptr, this, XCreateFontCursor(dpy.ptr, cur))
+        windowCursorInitialized.put(this, true)
     }
 }
-fun Window.unsetCursor(dpy: Display, window: Window = this) {
-    XUndefineCursor(dpy.ptr, window)
-    windowCursorInitialized.put(window, false)
+public fun Window.unsetCursor(dpy: Display) {
+    XUndefineCursor(dpy.ptr, this)
+    windowCursorInitialized.put(this, false)
 }
 // TODO: Does this work with window focus?
-// Should these be part of Window if they can be used on windows other than themselves?
-// Or should `window` be removed and replaced with `this`?
 // https://tronche.com/gui/x/xlib/window/XUndefineCursor.html
